8b3e9e01c54c10dc32441de3754d6223b841f7bd
Mads Hartmann <mads379@gmail.com>
2022-09-06 19:11:26 +0000

    WIP

--- shell.nix
+++ shell.nix
@@ -11,6 +11,9 @@ mkShell {
 
   NIX_ENFORCE_PURITY = true;
 
+  # Setting DIRENV_LOG_FORMAT to the empty string means direnv won't output
+  # any logs when loading the environment. This makes things nice and quiet
+  # but if you need to debug things, temporarily removing it might be helpful.
   shellHook =
     ''
     export DIRENV_LOG_FORMAT=""
